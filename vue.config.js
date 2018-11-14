8a9b4f98d9a5531768acf77dd14d9a64daacb7c9
panwenhua <735535556@qq.com>
2018-11-14 14:06:41 +0800

    修改baseUrl

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ function resolve(dir) {
   return path.join(__dirname, dir);
 }
 module.exports = {
+  baseUrl: process.env.NODE_ENV === 'production' ? '/vue-calendar-list-view/' : '/',
   chainWebpack: config => {
     config.resolve.alias.set("@", resolve("src"))
   }
