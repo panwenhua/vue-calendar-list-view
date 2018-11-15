84e8ba6a675b3155ea7201906fff3bd4c840993a
panwenhua <735535556@qq.com>
2018-11-15 17:14:01 +0800

    去掉无用代码

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,3 @@
-const path = require("path");
-function resolve(dir) {
-  return path.join(__dirname, dir);
-}
 module.exports = {
   baseUrl: process.env.NODE_ENV === 'production' ? '/vue-calendar-list-view/' : '/',
-  chainWebpack: config => {
-    config.resolve.alias.set("@", resolve("src"))
-  }
 };
