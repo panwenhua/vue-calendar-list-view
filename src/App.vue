1e2b910825f751a177d798a281e82f80ec2336c4
panwenhua <735535556@qq.com>
2018-11-15 16:42:14 +0800

    测试travis

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,10 @@
 <script>
 import "@/assets/scss/common/common.scss";
 import Index from "@/views/index";
-import Examples from "@/views/example";
 export default {
   name: "app",
   components: {
-    Index,
-    Examples
+    Index
   }
 };
 </script>
