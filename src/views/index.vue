ba8ec27d03b933a5d5ecad5ee3a2edc2f75d27a2
panwenhua <735535556@qq.com>
2018-11-15 11:55:25 +0800

    发布新版本

--- src/views/index.vue
+++ src/views/index.vue
@@ -47,27 +47,27 @@ export default {
         options: [
           {
             //对应日期的  人数
-            date: "2018-11-11",
+            date: "2018-11-16",
             label: "剩12人",
             value: 12
           },
           {
-            date: "2018-11-12",
+            date: "2018-11-17",
             label: "剩13人",
             value: 13
           },
           {
-            date: "2018-11-13",
+            date: "2018-12-13",
             label: "剩14人",
             value: 14
           },
           {
-            date: "2018-11-14",
+            date: "2018-12-14",
             label: "剩15人",
             value: 15
           },
           {
-            date: "2018-11-20",
+            date: "2018-12-20",
             label: "剩16人",
             value: 16
           }
